0dfb2345442e32ae1d2082c4aab41b1351cdea6e
Lawrence-Windows <larryoatmeal@gmail.com>
2013-07-19 20:31:33 -0700

    Fixed evolutions mess

---
+++ musicstore3/conf/evolutions/backup/1.sql
@@ -0,0 +1,20 @@
+# --- !Ups
+
+USE musicstore;
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products
+(
+  id              int          NOT NULL,
+  name            varchar(255) NOT NULL,
+  price           int          NOT NULL,
+
+
+
+  PRIMARY KEY     (id)
+
+);
+
+
+# --- !Downs
+
